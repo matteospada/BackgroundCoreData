534e2e79108ee1d6600be7a9d54a27e030549a5f
Matteo Spada <matteo.spada@ymail.com>
2022-11-20 15:45:25 +0100

    Initial Commit

---
+++ BackgroundCoreData/BackgroundCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundCoreDataApp.swift
+//  BackgroundCoreData
+//
+//  Created by Matteo Spada on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
